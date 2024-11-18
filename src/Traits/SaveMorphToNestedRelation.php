b9542cf815299691ffcfe832985af545e957131c
Henrique-BF <henrybf10@hotmail.com>
2024-11-18 12:13:45 -0300

    Fix issue when old_morph_class is not a model class

--- src/Traits/SaveMorphToNestedRelation.php
+++ src/Traits/SaveMorphToNestedRelation.php
@@ -4,6 +4,7 @@ namespace Cfx\LaravelNestedAttributes\Traits;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Arr;
 
 // todo: support "Custom Polymorphic Types"
@@ -39,14 +40,20 @@ final class SaveMorphToNestedRelation extends PersistableNestedRelation
         $foreignKeyName = $this->relation->getForeignKeyName();
         $morphType = $this->relation->getMorphType();
 
-        $old_morph_class = Arr::get($this->old_data, $morphType);
-        $old_morph_id = Arr::get($this->old_data, $foreignKeyName);
+        $old_morphable_type = $this->model->getRawOriginal($morphType);
+        $old_morphable_id = Arr::get($this->old_data, $foreignKeyName);
 
-        if (! $old_morph_class || ! $old_morph_id) {
+        if (! $old_morphable_type || ! $old_morphable_id) {
             return null;
         }
 
-        return $old_morph_class::find($old_morph_id);
+        $morphable_class = Relation::getMorphedModel($old_morphable_type) ?? $old_morphable_type;
+
+        if (class_exists($morphable_class)) {
+            return $old_morphable_type::find($old_morphable_id);
+        }
+
+        throw "$old_morphable_type is not a valid model class";
     }
 
     private function createNewMorphTo()
